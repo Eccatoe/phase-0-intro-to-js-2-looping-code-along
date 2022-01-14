7e6747bd5256af3966cf3ac8ef4a8aa22fceed8d
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-13 19:29:21 -0500

    commit tree f32b0cb9218dd01fb4b9a196f3330274435a6ead

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const namesArray=["Guadalupe", "Ollie", "Aki"]
+
+const writeCards=(array, adjective)=>{
+    let thankYouCards=[]
+    for (let i=0; i<array.length; i++){
+        let name=namesArray[i]
+        let message=`Thank you, ${name}, for the wonderful ${adjective} gift!`
+        thankYouCards.push(message)
+        }
+    return thankYouCards
+}
+
+function countDown(num){
+    while (num>=0){
+        console.log(num--)
+    }
+}
